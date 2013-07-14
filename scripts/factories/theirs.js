a9979a990a12339ee466434347ded049f18f068e
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2013-07-14 11:42:23 -0300

    retrieving data from factory with http

--- scripts/factories/theirs.js
+++ scripts/factories/theirs.js
@@ -3,16 +3,14 @@
 angular.module('twitter')
   .factory('theirTwitterFactory', function($http){
     var factory = {};
-    var tweets  = [];
 
     $http.get('/data/theirs.json')
       .success(function(data, status, headers, config) {
-        console.log(data);
+        factory.tweets = data;
       });
 
     factory.getTheirsTweets = function(){
-      return tweets;
+      return factory.tweets;
     };
-
     return factory;
   });
